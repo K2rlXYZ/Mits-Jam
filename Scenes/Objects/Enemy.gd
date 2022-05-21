9a7e43ca574de05cb8186404c6f9c0de0bfba700
M1hkel <Mihkelmaspanov22@gmail.com>
2022-05-21 14:20:49 +0300

    fixed chase

--- Scenes/Objects/Enemy.gd
+++ Scenes/Objects/Enemy.gd
@@ -3,18 +3,30 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 var speed = 200
 var health = 100
+var chase = false
 
 func _ready():
 	pass 
 
 func _physics_process(delta):
-	velocity = Vector2.ZERO
-	var player = get_parent().get_parent().get_node_or_null("Player")
-	if player != null:
-		velocity = position.direction_to(player.position) * speed
-	velocity = move_and_slide(velocity)
-	
+	if chase ==  true:
+		velocity = Vector2.ZERO
+		var player = get_parent().get_parent().get_node_or_null("Player")
+		if player != null:
+			velocity = position.direction_to(player.position) * speed
+		velocity = move_and_slide(velocity)
+		
 	
 	
 	if health <= 0:
 		queue_free()
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		print("pede")
+		chase = true
+
+func _on_Area2D_body_exited(body):
+	if body.name == "Player":
+		chase = false
--- Scenes/Weapons/Cat_weapon.gd
+++ Scenes/Weapons/Cat_weapon.gd
@@ -30,7 +30,7 @@ func _physics_process(delta):
 func attack():
 	if can_shoot:
 		var furb = furb_preload.instance()
-		get_tree().get_rosot().get_node(Globals.level).add_child(furb)
+		get_tree().get_root().get_node(Globals.level).add_child(furb)
 		vec = (get_global_mouse_position()-global_position).normalized()
 		var pos = global_position
 		pos.x += 80*vec.x
