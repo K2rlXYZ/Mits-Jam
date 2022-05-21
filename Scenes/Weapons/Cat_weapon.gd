05ae3570b2d9709514d1540935dd00373ab51130
K2rlXYZ <fanbla345@gmail.com>
2022-05-21 11:24:05 +0300

    shooting cat

--- Scenes/Weapons/Cat_weapon.gd
+++ Scenes/Weapons/Cat_weapon.gd
@@ -7,6 +7,9 @@ extends StaticBody2D
 
 export(Texture) var idle
 export(Texture) var picked_up
+var vec = Vector2.ZERO
+
+var furb_preload = preload("res://Scenes/Weapons/furball.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,14 +25,23 @@ var damage = 20
 func _physics_process(delta):
 	pass
 	
+	
 func attack():
-	for enemy in get_tree().get_nodes_in_group("Enemies"):
-		var dist = enemy.global_position.distance_to(global_position)
-		if dist < 100:
-			enemy.health-=damage
+	var furb = furb_preload.instance()
+	get_tree().get_root().get_node("Level1").add_child(furb)
+	vec = (get_global_mouse_position()-global_position).normalized()
+	var pos = global_position
+	pos.x += 80*vec.x
+	pos.y += 80*vec.y
+	furb.position = pos
+	furb.shoot(get_global_mouse_position())
 
 func change_state():
 	if $Sprite.texture == idle:
 		$Sprite.texture = picked_up
 	else:
 		$Sprite.texture = idle
+
+
+func _on_Timer_timeout():
+	pass # Replace with function body.
---
+++ Scenes/Weapons/furball.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+export var fly_speed = 15
+
+var dir = Vector2.ZERO
+var dieing = false
+
+
+func _ready():
+	pass
+	
+func _physics_process(delta):
+	position += dir * fly_speed
+
+func shoot(towards):
+	$Sprite.visible = true
+	$CollisionShape2D.disabled = false
+	dir = (towards - position).normalized()
+	
+	
+
+
+func _on_furball_body_entered(body):
+	var pl = get_node_or_null("/root/Level1/Player")
+	if body.is_in_group("Enemies"):
+		body.health-=25
+		dieing = true
+		destroy()
+	elif pl == body:
+		pass
+	else:
+		dieing = true
+		destroy()
+			
+func destroy():
+	queue_free()
