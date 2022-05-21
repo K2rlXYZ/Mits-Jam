103cfe40620152a3590b8d65ed2f82b1d393c19b
OskarU <oskar.unn2003@gmail.com>
2022-05-21 21:13:19 +0300

    QOL

--- Singletons/Globals.gd
+++ Singletons/Globals.gd
@@ -14,3 +14,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _input(event):
+	if event.is_action_pressed("toggle_fullscreen"):
+		OS.window_fullscreen = !OS.window_fullscreen
