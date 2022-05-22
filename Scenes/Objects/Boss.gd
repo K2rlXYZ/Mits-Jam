796c23faf4f7734e02add0a09917c4b2cb1c61a8
K2rlXYZ <fanbla345@gmail.com>
2022-05-22 13:46:28 +0300

    Update Boss.gd

--- Scenes/Objects/Boss.gd
+++ Scenes/Objects/Boss.gd
@@ -45,7 +45,7 @@ func _physics_process(delta):
 			get_node("AnimationPlayer").play("right_swing")
 	
 	if health <= 0:
-		var blood = load("res://Scenes/Objects/Blood.tscn").instance()
+		var blood = load("res://Scenes/Objects/BloodBoss.tscn").instance()
 		SceneHandler.current_level.add_child(blood)
 		blood.global_position = global_position
 		blood.rotation = global_position.angle_to_point(SceneHandler.current_level.get_node_or_null("Player").global_position)
